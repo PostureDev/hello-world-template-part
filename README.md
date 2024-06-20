8592ffb842bc68cd62a4ac65b24199aaffec8d7c
Kevin Garubba <kevin.garubba@getposture.com>
2024-06-20 16:28:55 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hello World Template Part
+This is just an MVP to test dynamically loading libraries from packagist into wordpress themes using composer
---
+++ src/template-parts/hello-world.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Template part for displaying a message that says "Hello, World!"
+ */
+?>
+<h2 data-attribute="hello-world-posture-composer-package" style="color: blue;" >Hello world!</h2>
